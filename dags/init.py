bfa41ce46084f698be3e2c0305aeb3566a68622b
smajy <sma.jahanmir@gmail.com>
2023-05-05 18:00:49 +0330

    phase data project

--- dags/init.py
+++ dags/init.py
@@ -15,18 +15,18 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('spark_job', default_args=default_args, description='A simple DAG to run a Spark job', schedule=None)
+dag = DAG('job', default_args=default_args, description='A simple DAG to run a Spark job', schedule=None)
 
 # Define tasks as operators
 task1 = BashOperator(
     task_id='task1',
-    bash_command='echo "Task 1: Load data from CSV"',
+    bash_command='echo "Task 1: Extract data"',
     dag=dag,
 )
 
 task2 = PythonOperator(
     task_id='task2',
-    python_callable=read_csv,  # Call your Spark job function
+    python_callable=ExtractData,  # Call your Spark job function
     dag=dag,
 )
 
@@ -36,31 +36,24 @@ task3 = BashOperator(
     dag=dag,
 )
 
-task4 = BashOperator(
+
+task4 = PythonOperator(
     task_id='task4',
-    bash_command='echo "Task 4: Save transformed data"',
+    python_callable=TransformData,  # Call your Spark job function
+    dag=dag,
+)
+
+task5 = BashOperator(
+    task_id='task5',
+    bash_command='echo "Task 4: Load data"',
+    dag=dag,
+)
+
+task6 = PythonOperator(
+    task_id='task4',
+    python_callable=LoadData,  # Call your Spark job function
     dag=dag,
 )
 
 # Define task dependencies
-task1 >> task2 >> task3 >> task4
-
-# t1 = BashOperator(
-#     task_id='print_date',
-#     bash_command='date',
-#     dag=dag,
-# )
-# from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
-# t2 = SparkSubmitOperator(
-#     task_id='run_spark_job',
-#     application='/path/to/spark/job.jar',
-#     name='Spark Job',
-#     conn_id='spark_default',
-#     conf={'master': 'yarn', 'deploy-mode': 'cluster'},
-#     executor_memory='2g',
-#     executor_cores=1,
-#     num_executors=2,
-#     application_args=['input.csv', 'output.parquet'],
-#     dag=dag,
-# )
-# t1 >> t2
+task1 >> task2 >> task3 >> task4 >> task5 >> task6
